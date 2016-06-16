09188bcff6569a1175292e7cb577efee00135411
zaragozamartin91 <zaragozamartin91@gmail.com>
2016-06-16 00:04:11 -0300

    se agrego el modelo de bandas, falta probarlo

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,13 +6,12 @@ SE CONECTA A LA BBDD*/
 var mongoose = require('mongoose');
 var appConfig = require('./appConfig');
 
-/*config carga las variables de configuracion apropiadas de acuerdo al ambiente (por ejemplo, si NODE_ENV es 'development',
-entonces carga configs de ./env/development.js). config.db tiene la url de la bbdd.*/
 
 module.exports = function() {
     var db = mongoose.connect(appConfig.db);
 
-    require('../models/user.server.model.js');
+    require('../models/user.server.model.js').registerSchema();
+    require('../models/band.server.model.js').registerSchema();
 
     return db;
 };
---
+++ models/band.server.model.js
@@ -0,0 +1,41 @@
+/*DEFINE EL SCHEMA Y REGISTRA EL MODELO DE USUARIO */
+// -----------------------------------------------------------------
+
+exports.registerSchema = function() {
+    var mongoose = require('mongoose');
+    var Schema = mongoose.Schema;
+
+    var BandSchema = new Schema({
+        name: String,
+        /*The created date field should be initialized at creation time and save the time the user document
+        was initially created*/
+        created: {
+            type: Date,
+            default: Date.now
+        },
+        members: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    });
+
+    /*A post middleware is defined using the post() method of the schema object*/
+    /*esta funcion correra despues de ejecutar save() sobre mongo.*/
+    BandSchema.post('save', function(next) {
+        if (this.isNew) {
+            console.log('A new band was created!');
+        } else {
+            console.log('An band was updated!');
+        }
+    });
+
+    /*This will force Mongoose to include getters when converting the MongoDB document to a JSON representation and will allow the
+    output of documents using res.json(). Tambien habilita los campos virtuales como fullName.*/
+    BandSchema.set('toJSON', {
+        getters: true,
+        virtuals: true
+    });
+
+    console.log("registering Band model!");
+    mongoose.model('Band', BandSchema);
+};
