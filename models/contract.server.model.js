2abf3f699bf7fa34c1e38a3e805cb7b37fa831cf
zaragozamartin91 <zaragozamartin91@gmail.com>
2016-07-03 00:53:41 -0300

    Se comenzo a trabajar en un modelo de contratos entre una banda y un lugar. La pantalla de creacion de contratos tal vez no funcione pero lo subo de todos modos para que no quede pendiente.

---
+++ models/contract.server.model.js
@@ -0,0 +1,70 @@
+/*DEFINE EL SCHEMA Y REGISTRA EL MODELO DE USUARIO */
+// -----------------------------------------------------------------
+
+/*considero que los nombres de bandas deben ser unicos...*/
+exports.registerSchema = function() {
+    var mongoose = require('mongoose');
+    var Schema = mongoose.Schema;
+
+    var BandSchema = new Schema({
+        band: {
+            type: Schema.Types.ObjectId,
+            ref: 'Band'
+        },
+        location: {
+            type: Schema.Types.ObjectId,
+            ref: 'Location'
+        },
+        /*Campo que guarda la fecha de creacion de la banda*/
+        created: {
+            type: Date,
+            default: Date.now
+        },
+        /*Campo que guarda la fecha del evento*/
+        eventDate: {
+            type: Date,
+            default: Date.now
+        },
+        /*Campo que guarda la fecha de caducidad del evento*/
+        expirationDate: {
+            type: Date,
+            default: Date.now
+        },
+        description: {
+            type: String,
+            default: 'Descripcion no disponible'
+        },
+        cash: {
+            type: Number,
+            default: 0
+        }
+    });
+
+    /*busca una unica banda por nombre.*/
+    BandSchema.statics.findOneByName = function(name, callback) {
+        console.log("Buscando banda: " + name);
+        this.findOne({
+            name: name
+        }, callback);
+    };
+
+    /*A post middleware is defined using the post() method of the schema object*/
+    /*esta funcion correra despues de ejecutar save() sobre mongo.*/
+    BandSchema.post('save', function(next) {
+        if (this.isNew) {
+            console.log('Se creo una banda nueva!');
+        } else {
+            console.log('Se actualizo una banda!');
+        }
+    });
+
+    /*This will force Mongoose to include getters when converting the MongoDB document to a JSON representation and will allow the
+    output of documents using res.json(). Tambien habilita los campos virtuales como fullName.*/
+    BandSchema.set('toJSON', {
+        getters: true,
+        virtuals: true
+    });
+
+    console.log("Registrando modelo de bandas!");
+    mongoose.model('Band', BandSchema);
+};
