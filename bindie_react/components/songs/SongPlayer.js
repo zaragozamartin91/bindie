afa1e55f19e997a1a92567535140df646faa48cb
zaragozamartin91 <zaragozamartin91@gmail.com>
2017-07-30 15:12:56 -0300

    Se agregaron los botones de anterior y siguiente cancion

--- bindie_react/components/songs/SongPlayer.js
+++ bindie_react/components/songs/SongPlayer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import FlatButton from 'material-ui/FlatButton';
+import PlayNextIcon from 'material-ui/svg-icons/av/skip-next';
+import PlayPreviousIcon from 'material-ui/svg-icons/av/skip-previous';
+
 const SongPlayer = React.createClass({
     /** song: Nombre del archivo de cancion a reproducir. La cancion se buscara en /public/songs/
      * nextSong: Callback a invocar para reproducir la siguiente cancion.
@@ -34,15 +38,38 @@ const SongPlayer = React.createClass({
             console.log("RENDERING SongPlayer!");
             let src = `/songs/${this.props.song}`;
 
+            const isFirefox = typeof InstallTrigger !== 'undefined';
+            const audioCtrlStyle = {
+                width: "50%",
+                borderRadius: 0
+            };
+            if (isFirefox) {
+                audioCtrlStyle.backgroundColor = "#484848";
+                audioCtrlStyle.color = "#FFFFFF";
+            } else {
+                audioCtrlStyle.backgroundColor = "#fafafa";
+                audioCtrlStyle.color = "#5a5a5a";
+            }
+
             return (
-                <audio
-                    ref={audio => this.audio = audio}
-                    onEnded={this.onSongEnd}
-                    style={{ width: "100%" }}
-                    controls>
-                    <source src={src} type="audio/mpeg" />
-                    <p>Your browser does not support the audio element.</p>
-                </audio>
+                <div style={{ width: "100%" }}>
+                    <audio
+                        ref={audio => this.audio = audio}
+                        onEnded={this.onSongEnd}
+                        style={{ width: "100%" }}
+                        controls>
+                        <source src={src} type="audio/mpeg" />
+                        <p>Your browser does not support the audio element.</p>
+                    </audio>
+                    <div style={{ width: "100%" }}>
+                        <FlatButton
+                            icon={<PlayNextIcon />}
+                            style={audioCtrlStyle} />
+                        <FlatButton
+                            icon={<PlayPreviousIcon />}
+                            style={audioCtrlStyle} />
+                    </div>
+                </div>
             )
         }
     }
