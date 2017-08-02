06408612120bd8e6614365c462caecb0eb8b2d0e
zaragozamartin91 <zaragozamartin91@gmail.com>
2017-08-02 20:35:29 -0300

    Se logro restringir el acceso a la musica

--- bindie_react/routes/view-routes.js
+++ bindie_react/routes/view-routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var url = require('url');
 
 /* GET home page. */
 router.get('/main', function (req, res, next) {
@@ -9,8 +10,17 @@ router.get('/main', function (req, res, next) {
 
 router.get('/sample', function (req, res) {
   console.log(req.headers);
-  console.log(`REFERER: ${req.headers.referer || "UNDEFINED"}`);
-  res.sendFile(path.join(__dirname, '0956.ogg'));
+
+  if (req.headers.referer) {
+    console.log(`REFERER: ${req.headers.referer}`);
+    let pathname = url.parse(req.headers.referer).pathname;
+    console.log(`PATHNAME: ${pathname}`);
+
+    if (pathname == "/main") res.sendFile(path.join(__dirname, '0956.ogg'));
+    else res.end("NO DEBERIAS ESTAR AQUI");
+  } else {
+    res.end("NO DEBERIAS ESTAR AQUI");
+  }
 });
 
 module.exports = router;
