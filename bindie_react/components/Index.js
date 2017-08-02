67a6ad8782752b0d7f89126a43c07f217793d4e2
zaragozamartin91 <zaragozamartin91@gmail.com>
2017-08-02 20:18:48 -0300

    Se agrego un ejemplo de transferencia de audio usando http requests

--- bindie_react/components/Index.js
+++ bindie_react/components/Index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 var Index = React.createClass({
-    render: function() {
-        return <h1>Bienvenido a Bindie!</h1>;
+    render: function () {
+        return (
+            <div>
+                <h1>Bienvenido a Bindie!</h1>
+                <audio
+                    style={{ width: "50%" }}
+                    controls>
+                    <source src="/sample" type="audio/ogg" />
+                    <p>Your browser does not support the audio element.</p>
+                </audio>
+            </div>
+        );
     }
 });
 
--- bindie_react/routes/view-routes.js
+++ bindie_react/routes/view-routes.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/main', function (req, res, next) {
   res.render('index', { title: 'Bindie' });
 });
 
-
+router.get('/sample', function (req, res) {
+  console.log(req.headers);
+  console.log(`REFERER: ${req.headers.referer || "UNDEFINED"}`);
+  res.sendFile(path.join(__dirname, '0956.ogg'));
+});
 
 module.exports = router;
