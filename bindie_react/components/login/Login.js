6daec74613e20892a0095dcfa20f96ebeb65dee5
Martin <zaragozamartin91@gmail.com>
2017-08-08 09:40:27 -0300

    Se sube una version de la pantalla de login usando cards en vez de paper

--- bindie_react/components/login/Login.js
+++ bindie_react/components/login/Login.js
@@ -4,7 +4,9 @@ import ReactDom from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-import Paper from 'material-ui/Paper';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
 
 /* ESTE FRAGMENTO DE CODIGO ES REQUERIDO PARA LOS EVENTOS DE TIPO TOUCH O CLICK EN COMPONENTES MATERIAL-UI */
 var injectTapEventPlugin = require("react-tap-event-plugin");
@@ -14,32 +16,43 @@ injectTapEventPlugin();
 const Login = React.createClass({
     submitForm: function () {
         console.log("Submiting form");
-        this.form.submit();
+        if (this.form) this.form.submit();
     },
 
     render: function () {
         return (
-            <MuiThemeProvider>
-                <Paper zDepth={2} >
-                    <form method="POST" action="/login" ref={f => { this.form = f }}>
-                        <TextField
-                            name="email"
-                            hint="email"
-                            floatingLabelText="email" /><br/>
+            <div>
+                <h1>Bindie</h1>
+                <MuiThemeProvider>
+                    <Card>
+                        <CardHeader
+                            title="Iniciar sesion"
+                            subtitle="Inicia sesion con una cuenta existente en BINDIE."
+                            actAsExpander={true}
+                            showExpandableButton={true}
+                        />
 
-                        <TextField
-                            name="password"
-                            hintText="Password"
-                            floatingLabelText="Password"
-                            type="password" /><br/>
+                        <CardText expandable={true}>
+                            <form method="POST" action="/login" ref={f => { this.form = f }}>
+                                <TextField
+                                    name="email"
+                                    hint="email"
+                                    floatingLabelText="email" /><br />
 
-                        <RaisedButton
-                            label="Iniciar sesion"
-                            onClick={this.submitForm}
-                            primary={true} />
-                    </form>
-                </Paper>
-            </MuiThemeProvider >
+                                <TextField
+                                    name="password"
+                                    hintText="Password"
+                                    floatingLabelText="Password"
+                                    type="password" /><br />
+                            </form>
+                        </CardText>
+                        <CardActions>
+                            <FlatButton label="Iniciar sesion" onClick={this.submitForm} />
+                        </CardActions>
+                    </Card>
+
+                </MuiThemeProvider >
+            </div>
         );
     }
 });
